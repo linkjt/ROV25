5972e0076c434a095ba4b5d366736f137ab00213
linkjt <158784232+linkjt@users.noreply.github.com>
2025-03-21 09:52:49 -0500

    Update gpio.h

--- gpio.h
+++ gpio.h
@@ -1,17 +1,2 @@
 #include <pigpio.h>
-void gpiostart(){
-  FILE *fptr;
-
-// Open a file in writing mode
-fptr = fopen("log.txt", "w");
-
-  if (gpioInitialise() < 0)
-{
-   fprintf(fptr, "Faileure Initilizing GPIO");
-}
-else
-{
-   fprintf(fptr, "GPIO initilized");
-}
-  fclose(fptr); 
-}
+void gpiostart()
