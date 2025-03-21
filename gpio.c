3b1f239e024df19df49f2964052e8f3cdbd0fbf4
linkjt <158784232+linkjt@users.noreply.github.com>
2025-03-21 09:53:14 -0500

    Update gpio.c

--- gpio.c
+++ gpio.c
@@ -1,1 +1,16 @@
+void gpiostart(){
+  FILE *fptr;
 
+// Open a file in writing mode
+fptr = fopen("log.txt", "w");
+
+  if (gpioInitialise() < 0)
+{
+   fprintf(fptr, "Faileure Initilizing GPIO");
+}
+else
+{
+   fprintf(fptr, "GPIO initilized");
+}
+  fclose(fptr); 
+}
