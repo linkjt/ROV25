9335b3b780f8d35e4483bbf1a1133b08c277ebf8
linkjt <158784232+linkjt@users.noreply.github.com>
2025-04-17 10:22:20 -0500

    Update main.c

--- main.c
+++ main.c
@@ -5,6 +5,10 @@
 #define ROVDOWN 2
 #define ROVX 3
 #define ROVY 4
+
+#define ARMPIN1 10
+#define ARMPIN2 11
+
 controller logi; // get this data from somewhere using the control data thing I made
 
 gpiostart();
@@ -12,9 +16,11 @@ gpioSetMode(ROVUP,PI_OUTPUT);
 gpioSetMode(ROVDOWN,PI_OUTPUT);
 gpioSetMode(ROVX,PI_OUTPUT);
 gpioSetMode(ROVY,PI_OUTPUT);
+gpioSetMode(ARMPIN1,PI_OUTPUT);
+gpioSetMode(ARMPIN2,PI_OUTPUT);
 while(1=1){
 
-
+// Maybe use a switch case, not rly sure rn, I JUST NEED TO KNOW WIREING
 gpioServo(ROVX, 1500);
 gpioServo(ROVY, 1500);
 if(logi.Dpadup){
@@ -37,5 +43,9 @@ if(logi.leftjoyY!=0){
 }else{
   gpioServo(ROVY, 1500);
 }
+//fidle around till controls are good
+gpioServo(ARMPIN1, 1500+logi.rightjoyY*10);
+//fidle around till controls are good
+gpioServo(ARMPIN2, 1500+logi.leftjoyY*10);
 
 
