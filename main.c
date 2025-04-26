cb5ba23d11bb3dccfda15aada271e9a4aa7aafd6
Vineeth <94882582+Vineethm0410@users.noreply.github.com>
2025-04-26 08:25:36 -0500

    updated code 4/26/25

--- main.c
+++ main.c
@@ -11,22 +11,21 @@
 #include <stdbool.h>
 #include <linux/joystick.h>
 
-
-#define UART_DEVICE "/dev/ttyS0" // if Uart not working, switch to AMA0, i dunno
+#define UART_DEVICE "/dev/ttyS0"
 #define BAUD_RATE B115200
-#define MAX_BUFFER_SIZE 512 // Increased buffer size to accommodate more data
+#define MAX_BUFFER_SIZE 512
 
-#define ROVUP1 0
-#define ROVUP2 2
-#define ROVM1 24
-#define ROVM2 25
-#define ROVM3 21
-#define ROVM4 22
+#define ROVUP1 17
+#define ROVUP2 27
+#define ROVM1 5
+#define ROVM2 6
+#define ROVM3 19
+#define ROVM4 26
 
-#define ARMPIN1 10
-#define ARMPIN2 11
+#define ARMPIN1 9
+#define ARMPIN2 10
 
-#define HANDCODE 9
+#define HANDCODE 11
 
 typedef struct {
     int rightjoyX;
@@ -45,9 +44,20 @@ typedef struct {
     bool Dpadleft;
     bool Dpadright;
     bool Dpaddown;
-} controller; //controller structure used
+} controller;
 
-// Function to read and update the controller state, just get based on the type of event and event number
+// Scale joystick axis input to a range of -500 to +500
+int scale_axis(int value) {
+    return (int)(value * 500.0 / 32767.0);
+}
+
+// Clamp a value between 500 and 2500 (for servo)
+int clamp_servo(int value) {
+    if (value < 500) return 500;
+    if (value > 2500) return 2500;
+    return value;
+}
+
 int update_logitech_state(int fd, controller *state) {
     struct js_event js;
     ssize_t bytes_read;
@@ -55,39 +65,42 @@ int update_logitech_state(int fd, controller *state) {
     bytes_read = read(fd, &js, sizeof(js));
 
     if (bytes_read == sizeof(js)) {
-        // type is event axis
         if (js.type & JS_EVENT_AXIS) {
             switch (js.number) {
-                case 0: state->leftjoyX = js.value; break;   
+                case 0: state->leftjoyX = js.value; break;
                 case 1: state->leftjoyY = -js.value; break;
                 case 2: state->rightjoyX = js.value; break;
                 case 3: state->rightjoyY = -js.value; break;
                 case 4: state->lefttrigger = js.value; break;
                 case 5: state->righttrigger = js.value; break;
+                case 6: // D-pad Left/Right
+                    state->Dpadleft = (js.value < 0);
+                    state->Dpadright = (js.value > 0);
+                    if (js.value == 0) {
+                        state->Dpadleft = false;
+                        state->Dpadright = false;
+                    }
+                    break;
+                case 7: // D-pad Up/Down
+                    state->Dpadup = (js.value < 0);
+                    state->Dpaddown = (js.value > 0);
+                    if (js.value == 0) {
+                        state->Dpadup = false;
+                        state->Dpaddown = false;
+                    }
+                    break;
             }
         } else if (js.type & JS_EVENT_BUTTON) {
             switch (js.number) {
-                // button values, have type event button 
-                case 3: state->Ybutton = js.value; break;    
-                case 2: state->Xbutton = js.value; break;   
-                case 1: state->Bbutton = js.value; break;     
-                case 0: state->Abutton = js.value; break;     
-                case 6: state->Backbutton = js.value; break; 
-                case 7: state->Startbutton = js.value; break; 
-            }
-            //Dpad values, all on number
-        } else if (js.type & JS_EVENT_HAT) {
-            switch (js.number) {
-                case 0:
-                    state->Dpadup = (js.value == -1 || js.value == -3); 
-                    state->Dpaddown = (js.value == 1 || js.value == 3);    
-                    state->Dpadleft = (js.value == -1 || js.value == -2);
-                    state->Dpadright = (js.value == 1 || js.value == 2);
-                    break;
+                case 3: state->Ybutton = js.value; break;
+                case 2: state->Xbutton = js.value; break;
+                case 1: state->Bbutton = js.value; break;
+                case 0: state->Abutton = js.value; break;
+                case 6: state->Backbutton = js.value; break;
+                case 7: state->Startbutton = js.value; break;
             }
         }
         return 1;
-        //error handeling
     } else if (bytes_read == -1 && (errno == EAGAIN || errno == EWOULDBLOCK)) {
         return 0;
     } else {
@@ -96,192 +109,68 @@ int update_logitech_state(int fd, controller *state) {
     }
 }
 
+void servomove(controller logi) {
+    int lx = scale_axis(logi.leftjoyX);
+    int ly = scale_axis(logi.leftjoyY);
+    int rx = scale_axis(logi.rightjoyX);
+    int ry = scale_axis(logi.rightjoyY);
 
-void servomove(controller logi){
+    int lt = scale_axis(logi.lefttrigger);
+    int rt = scale_axis(logi.righttrigger);
 
-// Maybe use a switch case, not rly sure rn, I JUST NEED TO KNOW WIREING
+    int boost = (1 + logi.Startbutton);
 
-gpioServo(ROVUP1, 1500+200*(logi.Dpadup)*(1+logi.Startbutton)-200*(logi.Dpaddown)*(1+logi.Startbutton)+50*(logi.Bbutton)-50*(logi.Xbutton));
-gpioServo(ROVUP2, 1500+200*(logi.Dpadup)*(1+logi.Startbutton)-200*(logi.Dpaddown)*(1+logi.Startbutton)+50*(logi.Xbutton)-50*(logi.Bbutton));
-//X movement
-gpioServo(ROVM1, 1500+100*(logi.leftjoyX)*(1+logi.Startbutton)+100*(logi.leftjoyY)*(1+logi.Startbutton)+50*(logi.Ybutton)-50*(logi.Abutton));
-gpioServo(ROVM2, 1500+100*(logi.leftjoyX)*(1+logi.Startbutton)-100*(logi.leftjoyY)*(1+logi.Startbutton)+50*(logi.Ybutton)-50*(logi.Abutton));
-gpioServo(ROVM3, 1500-100*(logi.leftjoyX)*(1+logi.Startbutton)+100*(logi.leftjoyY)*(1+logi.Startbutton)+50*(logi.Ybutton)-50*(logi.Abutton));
-gpioServo(ROVM4, 1500-100*(logi.leftjoyX)*(1+logi.Startbutton)-100*(logi.leftjoyY)*(1+logi.Startbutton)+50*(logi.Ybutton)-50*(logi.Abutton));
+    gpioServo(ROVUP1, clamp_servo(1500 + 100 * logi.Dpadup * boost - 100 * logi.Dpaddown * boost + 50 * logi.Bbutton - 50 * logi.Xbutton));
+    gpioServo(ROVUP2, clamp_servo(1500 + 100 * logi.Dpadup * boost - 100 * logi.Dpaddown * boost + 50 * logi.Xbutton - 50 * logi.Bbutton));
 
-gpioServo(HANDCODE, 1500+200*(logi.lefttrigger)*(1+logi.Startbutton)-200*(logi.righttrigger)*(1+logi.Startbutton));
-//fidle around till controls are good
-gpioServo(ARMPIN1, 1500+logi.rightjoyY*10*(1+logi.Startbutton));
-//fidle around till controls are good
-gpioServo(ARMPIN2, 1500+logi.rightjoyX*10*(1+logi.Startbutton));
+    gpioServo(ROVM1, clamp_servo(1500 + lx * boost + ly * boost + 50 * logi.Ybutton - 50 * logi.Abutton));
+    gpioServo(ROVM2, clamp_servo(1500 + lx * boost - ly * boost + 50 * logi.Ybutton - 50 * logi.Abutton));
+    gpioServo(ROVM3, clamp_servo(1500 - lx * boost + ly * boost + 50 * logi.Ybutton - 50 * logi.Abutton));
+    gpioServo(ROVM4, clamp_servo(1500 - lx * boost - ly * boost + 50 * logi.Ybutton - 50 * logi.Abutton));
+
+    gpioServo(HANDCODE, clamp_servo(1500 + lt * boost - rt * boost));
+
+    gpioServo(ARMPIN1, clamp_servo(1500 + ry * boost));
+    gpioServo(ARMPIN2, clamp_servo(1500 + rx * boost));
 }
 
-
 int main() {
-gpioInitialise();
-    // U may get an error syaing that GPIO failed to initlazise, this can be for multiple reasons but the most common is that it failed due to the fact that the robot 
-    // wasn't able to latch on to the current process so run sudo killall pigpiod
-    // this kills the dameon process and now u can run the code again
+    if (gpioInitialise() < 0) {
+        fprintf(stderr, "Failed to initialize GPIO\n");
+        return 1;
+    }
 
-int fd;
+    int fd;
     controller logi_state = {0};
-    const char *device_path = "/dev/input/js0"; // MAY NEED TO BE CHANGED
-    // If wegetting an error intilaizing USB run, sudo apt install evtest
-    // then run sudo evtest and it will show all current USB deivces so make sure that you remeber the Logi tech's one and then change this value to match ts
+    controller logi;
+
+    const char *device_path = "/dev/input/js0";
     fd = open(device_path, O_RDONLY | O_NONBLOCK);
     if (fd == -1) {
         perror("Error opening joystick");
-        exit(EXIT_FAILURE);
-    }
-controller logi;
-gpioSetMode(ROVUP1,PI_OUTPUT);
-gpioSetMode(ROVUP2,PI_OUTPUT);
-
-gpioSetMode(ROVM1,PI_OUTPUT);
-gpioSetMode(ROVM2,PI_OUTPUT);
-gpioSetMode(ROVM3,PI_OUTPUT);
-gpioSetMode(ROVM4,PI_OUTPUT);
-
-
-gpioSetMode(ARMPIN1,PI_OUTPUT);
-gpioSetMode(ARMPIN2,PI_OUTPUT);
-
-gpioSetMode(HANDCODE,PI_OUTPUT);
-
-
-    
-/*    int uart_fd;
-    struct termios options;
-    char buffer[MAX_BUFFER_SIZE];
-    int buffer_index = 0;
-
-    uart_fd = open(UART_DEVICE, O_RDWR | O_NOCTTY);
-    if (uart_fd < 0) {
-        perror("Error opening UART");
-        return -1;
+        return 1;
     }
 
-    if (tcgetattr(uart_fd, &options) < 0) {
-        perror("Error getting termios settings");
-        close(uart_fd);
-        return -1;
-    }
+    gpioSetMode(ROVUP1, PI_OUTPUT);
+    gpioSetMode(ROVUP2, PI_OUTPUT);
+    gpioSetMode(ROVM1, PI_OUTPUT);
+    gpioSetMode(ROVM2, PI_OUTPUT);
+    gpioSetMode(ROVM3, PI_OUTPUT);
+    gpioSetMode(ROVM4, PI_OUTPUT);
+    gpioSetMode(ARMPIN1, PI_OUTPUT);
+    gpioSetMode(ARMPIN2, PI_OUTPUT);
+    gpioSetMode(HANDCODE, PI_OUTPUT);
 
-    // Set the baud rate, make sure ts same as arduino
-    options.c_cflag = BAUD_RATE | CS8 | CLOCAL | CREAD;
-    options.c_iflag = 0;
-    options.c_oflag = 0;
-    options.c_lflag = 0;
-
-    tcflush(uart_fd, TCIFLUSH);
-    if (tcsetattr(uart_fd, TCSANOW, &options) < 0) {
-        perror("Error setting termios settings");
-        close(uart_fd);
-        return -1;
-    }
-
-    printf("Listening for controller data on %s at %d baud...\n", UART_DEVICE, BAUD_RATE);
-*/
     while (1) {
+        memset(&logi_state, 0, sizeof(controller));
 
-    controller.rightjoyX =0;
-    controller.rightjoyY =0;
-    controller.leftjoyX =0;
-    controller.leftjoyY =0;
-    controller.lefttrigger =0;
-    controller.righttrigger =0;
-    controller.Ybutton =false;
-    controller.Xbutton =false;
-    controller.Bbutton =false;
-    controller.Abutton =false;
-    controller.Backbutton =false;
-    controller.Startbutton =false;
-    controller.Dpadup =false;
-    controller.Dpadleft =false;
-    controller.Dpadright =false;
-    controller.Dpaddown =false;
-
-
-
-
-
-
-
-        
-        /*
-        int bytes_read = read(uart_fd, buffer + buffer_index, MAX_BUFFER_SIZE - 1 - buffer_index);
-        if (bytes_read > 0) {
-            buffer_index += bytes_read;
-            buffer[buffer_index] = '\0';
-
-            char *newline_pos;
-            while ((newline_pos = strchr(buffer, '\n')) != NULL) {
-                *newline_pos = '\0';
-
-                controller receivedLogi;
-                char *token;
-                char *rest = buffer;
-                int i = 0;
-                bool parse_error = false;
-
-                while ((token = strtok_r(rest, ",", &rest)) != NULL) {
-                    int value = atoi(token);
-                    bool bool_value = (strcmp(token, "true") == 0 || strcmp(token, "1") == 0);
-
-                    switch (i) {
-                        case 0: receivedLogi.rightjoyX = value; break;
-                        case 1: receivedLogi.rightjoyY = value; break;
-                        case 2: receivedLogi.leftjoyX = value; break;
-                        case 3: receivedLogi.leftjoyY = value; break;
-                        case 4: receivedLogi.lefttrigger = value; break;
-                        case 5: receivedLogi.righttrigger = value; break;
-                        case 6: receivedLogi.Ybutton = bool_value; break;
-                        case 7: receivedLogi.Xbutton = bool_value; break;
-                        case 8: receivedLogi.Bbutton = bool_value; break;
-                        case 9: receivedLogi.Abutton = bool_value; break;
-                        case 10: receivedLogi.Backbutton = bool_value; break;
-                        case 11: receivedLogi.Startbutton = bool_value; break;
-                        case 12: receivedLogi.Dpadup = bool_value; break;
-                        case 13: receivedLogi.Dpadleft = bool_value; break;
-                        case 14: receivedLogi.Dpadright = bool_value; break;
-                        case 15: receivedLogi.Dpaddown = bool_value; break;
-                        default: fprintf(stderr, "Error: Too many elements received: %s\n", buffer); parse_error = true; break;
-                    }
-                    i++;
-                }
-
-                if (!parse_error && i == 16) {
-                    logi = receivedLogi; 
-                    printf("Received Controller Data:\n");
-                    printf("  RX: %d, RY: %d, LX: %d, LY: %d\n", logi.rightjoyX, logi.rightjoyY, logi.leftjoyX, logi.leftjoyY);
-                    printf("  LT: %d, RT: %d\n", logi.lefttrigger, logi.righttrigger);
-                    printf("  Y: %d, X: %d, B: %d, A: %d\n", logi.Ybutton, logi.Xbutton, logi.Bbutton, logi.Abutton);
-                    printf("  Back: %d, Start: %d, Mode: %d\n", logi.Backbutton, logi.Startbutton, logi.Startbutton);
-                    printf("  DU: %d, DL: %d, DR: %d, DD: %d\n", logi.Dpadup, logi.Dpadleft, logi.Dpadright, logi.Dpaddown);
-                } else if (!parse_error) {
-                    fprintf(stderr, "Error: Incorrect number of controller elements received: %s (%d received, expected 17)\n", buffer, i);
-                }
-
-                int remaining_len = strlen(newline_pos + 1);
-                memmove(buffer, newline_pos + 1, remaining_len);
-                buffer_index = remaining_len;
-                buffer[buffer_index] = '\0';
-            }
-        } else if (bytes_read < 0) {
-            perror("Error reading from UART");
-            break;
-        }
-        usleep(100);
-        */
         if (update_logitech_state(fd, &logi_state) > 0) {
+            logi = logi_state;
             servomove(logi);
         }
-        usleep(50000);
+        usleep(50000); // 50ms
     }
 
-    //close(uart_fd);
     close(fd);
     return 0;
 }
-
-
